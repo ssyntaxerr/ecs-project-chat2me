b383baae26eb37c661542ddb6c50d18e7d22f77c
ssyntaxer <ssyntaxerr@gmail.com>
2026-03-11 15:35:41 +0200

    Update infra

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "c2m-terraform-state"
+    bucket = "c2mapp-terraform-state"
     key = "terraform.tfstate"
     region = "eu-north-1"
     use_lockfile = true
--- infra/variables.tf
+++ infra/variables.tf
@@ -27,3 +27,9 @@ variable "vpc_cidr" {
     type = string
     default = "10.0.0.0/16"
 }
+
+variable "image_tag" {
+    description = "Docker image tag"
+    type = string
+    default = "latest"
+}
