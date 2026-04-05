ce23d5a75c0fc70feb0c73e8604e3cd5825fdee2
ssyntaxer <ssyntaxerr@gmail.com>
2026-04-05 17:12:49 +0200

    Add ECS autoscaling

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -107,4 +107,49 @@ resource "aws_ecs_service" "service" {
   }
 }
 
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity = 3
+  min_capacity = 1
+  resource_id = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace =   "ecs"
+}
+
+resource "aws_appautoscaling_policy" "cpu_policy" {
+  name               = "${var.project_name}-cpu-scaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value = 70.0
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
+  }
+}
+
+resource "aws_appautoscaling_policy" "memory_policy" {
+  name               = "${var.project_name}-memory-scaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value = 70.0
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
+  }
+}
 
